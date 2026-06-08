d178365ffc7966a0a51e3b7e977c63af47c14d1d
devNish017 <nishantdwivedi017@gmail.com>
2026-06-08 20:57:24 +0530

    Change Redis client configuration
    Updated Redis client configuration with new credentials and host.

--- config/redis.js
+++ config/redis.js
@@ -2,10 +2,10 @@ import { createClient } from 'redis';
 
 const client = createClient({
     username: 'default',
-    password: 'llrT0dqkfb9AyVQGbbyfjfEHVZ2ItAXF',
+    password: 'ncf6WTXaSwKGvJzoKMHTDUpObQ0yMA7o',
     socket: {
-        host: 'redis-13995.crce214.us-east-1-3.ec2.cloud.redislabs.com',
-        port: 13995
+        host: 'redis-18614.c321.us-east-1-2.ec2.cloud.redislabs.com',
+        port: 18614
     }
 });
 
